87841417a36f488abb15871b103e8f02bae46c8f
sst <702451063@qq.com>
2021-05-15 15:35:11 +0800

    2-2 init

---
+++ src/main/java/com/nwpu/npu_wiki/NpuWikiApplication.java
@@ -0,0 +1,13 @@
+package com.nwpu.npu_wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpuWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpuWikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nwpu/npu_wiki/NpuWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwpu.npu_wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpuWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
