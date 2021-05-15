ac3f85fdb0303726f5dc93119c7a96514866d47f
sunsitong-Git <702451063@qq.com>
2021-05-15 16:57:40 +0800

    2-5 修改启动文案

--- src/main/java/com/nwpu/npu_wiki/NpuWikiApplication.java
+++ src/main/java/com/nwpu/npu_wiki/NpuWikiApplication.java
@@ -1,13 +1,21 @@
 package com.nwpu.npu_wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class NpuWikiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NpuWikiApplication.class, args);
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(NpuWikiApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(NpuWikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
 
 }
