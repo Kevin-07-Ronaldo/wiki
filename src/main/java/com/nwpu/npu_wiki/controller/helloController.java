9099e5525d9f65183787270d85c20a2bf5ad9efa
sunsitong-Git <702451063@qq.com>
2021-05-15 20:38:38 +0800

    2-8 配置环境 2-9 热部署

--- src/main/java/com/nwpu/npu_wiki/controller/helloController.java
+++ src/main/java/com/nwpu/npu_wiki/controller/helloController.java
@@ -1,5 +1,6 @@
 package com.nwpu.npu_wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class helloController {
+
+    @Value("${test.kd:default}")
+    private String string;
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String sayHello() {
-        return "Hello world!";
+        return "Hello world!" + string;
     }
 }
